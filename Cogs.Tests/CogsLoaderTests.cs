9059a15836b4b7032bab0772fbb07077744c32dd
Jeremy Iverson <jeremy@colectica.com>
2017-06-12 14:24:10 -0500

    Limited regression test for the model loader

--- Cogs.Tests/CogsLoaderTests.cs
+++ Cogs.Tests/CogsLoaderTests.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Cogs.Dto;
+using Cogs.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -8,9 +11,28 @@ namespace Cogs.Tests
     public class CogsLoaderTests
     {
         [Fact]
-        public void PassingTest()
+        public void LoadHamburgerModelTest()
         {
-            Assert.Equal(4, 3);
+            string path = "..\\..\\..\\..\\cogsburger";
+            var directoryReader = new CogsDirectoryReader();
+            var cogsDtoModel = directoryReader.Load(path);
+
+            var modelBuilder = new CogsModelBuilder();
+            var cogsModel = modelBuilder.Build(cogsDtoModel);
+
+            // Verify we read all the item types.
+            Assert.Equal(10, cogsModel.ItemTypes.Count);
+            Assert.Equal(1, cogsModel.ReusableDataTypes.Count);
+
+            // Verify we read all the inheritance correctly.
+            var rollType = cogsModel.ItemTypes.First(x => x.Name == "Roll");
+            Assert.Equal("Breading", rollType.ExtendsTypeName);
+
+            // Verify we read Topics correctly.
+            Assert.Equal(1, cogsModel.TopicIndices.Count);
+
+            // Verify we read properties correctly.
+
         }
     }
 }
