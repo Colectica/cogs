58e53bbf8316bce08d8e14d5ed8bf2179421baf9
Jeremy Iverson <jeremy@colectica.com>
2017-06-12 12:41:55 -0500

    Skeleton tests

---
+++ Cogs.Tests/CogsLoaderTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Cogs.Tests
+{
+    public class CogsLoaderTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 3);
+        }
+    }
+}
