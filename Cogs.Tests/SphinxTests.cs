6902275182780c93558944feb7dd51bbb9b65473
Jeremy Iverson <jeremy@colectica.com>
2017-06-12 14:30:46 -0500

    Test for SphinxPublisher

--- Cogs.Tests/SphinxTests.cs
+++ Cogs.Tests/SphinxTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Cogs.Dto;
+using Cogs.Model;
+using Cogs.Publishers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using Xunit;
 
 namespace Cogs.Tests
 {
-    class SphinxTests
+    public class SphinxTests
     {
+        [Fact]
+        public void SphinxForHamburgersTest()
+        {
+            string path = "..\\..\\..\\..\\cogsburger";
+
+            string subdir = Path.GetFileNameWithoutExtension(Path.GetTempFileName());
+            string outputPath = Path.Combine(Path.GetTempPath(), subdir);
+
+            var directoryReader = new CogsDirectoryReader();
+            var cogsDtoModel = directoryReader.Load(path);
+
+            var modelBuilder = new CogsModelBuilder();
+            var cogsModel = modelBuilder.Build(cogsDtoModel);
+
+            var sphinxPublisher = new SphinxPublisher();
+            sphinxPublisher.TargetDirectory = outputPath;
+            sphinxPublisher.Publish(cogsModel);
+
+
+            // TODO Inspect the sphinx directory to make sure it has some things we expect.
+            // For now we are just making sure there are no errors while running.
+
+
+        }
     }
 }
