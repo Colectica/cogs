6c212b1944be260fa56901799c81f72510edaa59
Kevin Christianson <christiansonk@carleton.edu>
2017-06-21 14:02:40 -0500

    imbeded nodes for dotSchema and adjusted umlschema to work with it

--- Cogs.Publishers/DotSchemaPublisher.cs
+++ Cogs.Publishers/DotSchemaPublisher.cs
@@ -40,7 +40,7 @@ namespace Cogs.Publishers
         public string Output { get; set; }
 
         private List<string> ClassList { get; set; }
-        private List<string> ReusableList { get; set; }
+        private List<DataType> ReusableList { get; set; }
 
         public void Publish(CogsModel model)
         {
@@ -56,20 +56,20 @@ namespace Cogs.Publishers
 
             Directory.CreateDirectory(TargetDirectory);
 
-            // create list of all classes so you know if a class is being referenced
+            // create list of all class names so you know if a class is being referenced
             ClassList = new List<string>();
             foreach (var item in model.ItemTypes.Concat(model.ReusableDataTypes))
             {
                 ClassList.Add(item.Name);
             }
-            // create list of all reusable types so you know if a reusable type is being referenced
-            ReusableList = new List<string>();
+            // create list of all reusable types so you know if a reusable type is being referenced and can get information about it
+            ReusableList = new List<DataType>();
             foreach (var item in model.ReusableDataTypes)
             {
-                ReusableList.Add(item.Name);
+                ReusableList.Add(item);
             }
-            var header = "digraph G { fontname = \"Bitstream Vera Sans\" fontsize = 8 node [fontname = \"Bitstream Vera Sans\" " +
-               "fontsize = 8 shape = \"record\"] edge [ fontname = \"Bitstream Vera Sans\" fontsize = 8 ] ";
+            var header = "digraph G { compound = true fontsize = 8 node [ " +
+               "fontsize = 8 shape = \"record\"] edge [ fontsize = 8 ] ";
 
             if (Output.Equals("all")) MakeGraphAll(model, header);
             else if (Output.Equals("topic")) MakeGraphTopic(model, header);
@@ -78,44 +78,81 @@ namespace Cogs.Publishers
 
         private string MakeItem(DataType item)
         {
-            var outputText = new StringBuilder();
-            var classText = item.Name + "[ label=\"{" + item.Name + "| ";
+            return MakeNode(item, item.Name + "[ label=\"{" + item.Name + "| ");
+        }
+
+        private string MakeCluster(DataType item, DataType reusable)
+        {
+            StringBuilder output = new StringBuilder();
+            StringBuilder outerClass = new StringBuilder("subgraph cluster" + item.Name + " { label=\"" + item.Name + "\" ");
+            List<DataType> reusablesPresent = new List<DataType>();
+            outerClass.Append(item.Name + "Properties [ label=\"{Properties |");
             foreach (var property in item.Properties)
             {
-                classText += property.Name + " : " + property.DataTypeName;
+                outerClass.Append(property.Name + " : " + property.DataTypeName);
                 if (!string.IsNullOrWhiteSpace(property.MinCardinality) && !string.IsNullOrWhiteSpace(property.MaxCardinality))
                 {
-                    classText += "[" + property.MinCardinality + "..." + property.MaxCardinality + "] ";
+                    outerClass.Append("[" + property.MinCardinality + "..." + property.MaxCardinality + "] ");
                 }
-                classText += "\\l";
+                outerClass.Append("\\l");
                 if (ClassList.Contains(property.DataTypeName))
                 {
-                    if (ReusableList.Contains(property.DataTypeName))
+                    if (ReusableList.Contains(property.DataType))
                     {
-                       // return MakeImbededNode(item);
-                        outputText.Append("edge[ arrowhead = \"none\" headlabel = \"0...1\" taillabel = \"0...1\"] ");
+                        reusablesPresent.Add(property.DataType);
                     }
                     else
                     {
-                        outputText.Append("edge[ arrowhead = \"none\" headlabel = \"0...*\" taillabel = \"0...*\"] ");
+                        output.Append("edge[ arrowhead = \"none\" headlabel = \"0...*\" taillabel = \"0...*\"] ");
                     }
-                    outputText.Append(item.Name + " -> " + property.DataTypeName + "[ label = \"" + property.Name + "\"] ");
+                    output.Append(item.Name + "Properties -> " + item.Name + property.DataTypeName + "[ label = \"" + property.Name + "\"] ");
                 }
             }
             if (!string.IsNullOrWhiteSpace(item.ExtendsTypeName))
             {
-                outputText.Append("edge [arrowhead = \"empty\"] ");
-                outputText.Append(item.Name + "->" + item.ExtendsTypeName + " ");
+                output.Append("edge [arrowhead = \"empty\"] ");
+                output.Append(item.Name + "Properties ->" + item.ExtendsTypeName + "[ltail=cluster" + item.Name + "] ");
             }
-            outputText.Append(classText + "}\"] ");
-            return outputText.ToString();
+            outerClass.Append("}\"] ");
+            foreach(var reused in reusablesPresent)
+            {
+                outerClass.Append(item.Name + reused.Name + " [ label=\"{ " + reused.Name + " | ");
+                foreach(var property in reused.Properties)
+                {
+                    outerClass.Append(property.Name + " : " + property.DataTypeName);
+                    if (!string.IsNullOrWhiteSpace(property.MinCardinality) && !string.IsNullOrWhiteSpace(property.MaxCardinality))
+                    {
+                        outerClass.Append("[" + property.MinCardinality + "..." + property.MaxCardinality + "] ");
+                    }
+                    outerClass.Append("\\l");
+                    if (ClassList.Contains(property.DataTypeName))
+                    {
+                        if (ReusableList.Contains(property.DataType))
+                        {
+                            reusablesPresent.Add(property.DataType);
+                        }
+                        else
+                        {
+                            output.Append("edge[ arrowhead = \"none\" headlabel = \"0...*\" taillabel = \"0...*\"] ");
+                        }
+                        output.Append(reused.Name + " -> " + property.DataTypeName + "[ label = \"" + property.Name + "\"] ");
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(reused.ExtendsTypeName))
+                {
+                    output.Append("edge [arrowhead = \"empty\"] ");
+                    output.Append(reused.Name + "->" + reused.ExtendsTypeName + " ");
+                }
+                outerClass.Append("}\"] ");
+            }
+            outerClass.Append("}");
+            output.Append(outerClass);
+            return output.ToString();
         }
 
-        private string MakeImbededNode(DataType item)
+        private string MakeNode(DataType item, string classText)
         {
-            var outputText = new StringBuilder();
-            var classText = "subgraph " + item.Name + "{ label=\"" + item.Name + "| ";
-            var innerClass = "";
+            StringBuilder outputText = new StringBuilder();
             foreach (var property in item.Properties)
             {
                 classText += property.Name + " : " + property.DataTypeName;
@@ -126,11 +163,10 @@ namespace Cogs.Publishers
                 classText += "\\l";
                 if (ClassList.Contains(property.DataTypeName))
                 {
-                    if (ReusableList.Contains(property.DataTypeName))
+                    if (ReusableList.Contains(property.DataType))
                     {
-                        innerClass = property.DataTypeName + "[ label=\"{" + property.DataTypeName + "| ";
-                        // create class for reusable type (maybe have reusableList contain actual classes rather than just names)
-                       // foreach(var prop in property.DataType)
+                        return MakeCluster(item, property.DataType);
+                        //  outputText.Append("edge[ arrowhead = \"none\" headlabel = \"0...1\" taillabel = \"0...1\"] ");
                     }
                     else
                     {
@@ -151,7 +187,7 @@ namespace Cogs.Publishers
         public void MakeGraphAll(CogsModel model, string header)
         {
             var output = new StringBuilder(header);
-            foreach (var item in model.ItemTypes.Concat(model.ReusableDataTypes))
+            foreach (var item in model.ItemTypes)
             {
                 output.Append(MakeItem(item));
             }
