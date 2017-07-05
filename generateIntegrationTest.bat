356a4ec24eb4b1351fdb4efd2458cb3df582fba6
Dan Smith <dan@colectica.com>
2017-07-05 14:19:29 -0500

    Add initial integration tests solution

---
+++ generateIntegrationTest.bat
@@ -0,0 +1,5 @@
+
+dotnet Cogs.Console\bin\Debug\netcoreapp2.0\Cogs.Console.dll publish-cs  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\netcoreapp2.0\Cogs.Console.dll publish-json  "cogsburger" "generated"
+
+@pause
