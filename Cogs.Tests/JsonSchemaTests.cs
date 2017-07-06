908b9893119cc1ba276a23fb681005daec31c335
Clement Tan <clementtph@gmail.com>
2017-07-06 10:24:32 -0500

    added test file

--- Cogs.Tests/JsonSchemaTests.cs
+++ Cogs.Tests/JsonSchemaTests.cs
@@ -17,14 +17,14 @@ namespace Cogs.Tests
         [Fact]
         public async System.Threading.Tasks.Task asyncJsonSchemaTestAsync()
         {
-            string path = "..\\..\\..\\..\\cogsburger";
+            string path = @"C:\Users\clement\Documents\GitHub\cogs\cogsburger";
 
             string subdir = Path.GetFileNameWithoutExtension(Path.GetTempFileName());
             string outputPath = Path.Combine(Path.GetTempPath(), subdir);
 
             var directoryReader = new CogsDirectoryReader();
             var cogsDtoModel = directoryReader.Load(path);
-            
+
             var modelBuilder = new CogsModelBuilder();
             var cogsModel = modelBuilder.Build(cogsDtoModel);
 
@@ -32,12 +32,28 @@ namespace Cogs.Tests
             jsonPublisher.TargetDirectory = outputPath;
             jsonPublisher.Publish(cogsModel);
 
-            var schemadata = File.ReadAllText(Path.Combine(outputPath, "jsonSchema" + ".json"));
-            var schema = await JsonSchema4.FromJsonAsync(schemadata);
-            var jsondata = File.ReadAllText(@"C: \Users\clement\Desktop\JsonFolder\testing1_reference_reusable.json");
-            var validate = schema.Validate(jsondata);
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                MetadataPropertyHandling = MetadataPropertyHandling.Ignore
+            };
+            var schemaData = File.ReadAllText(Path.Combine(outputPath, "jsonSchema" + ".json"));
+            var schema = await JsonSchema4.FromJsonAsync(schemaData);
 
-            Assert.Null(validate);
+            var jsondata1 = File.ReadAllText(@"..\Cogs.Tests.Console\testing1_reference_reusable.json");
+            var jsondata2 = File.ReadAllText(@"..\Cogs.Tests.Console\testing2_reference_Object.json");
+            var jsondata3 = File.ReadAllText(@"..\Cogs.Tests.Console\test3_SimpleType.json");
+            var jsondata4 = File.ReadAllText(@"..\Cogs.Tests.Console\test4_invalid_json.json");
+
+
+            var validate1 = schema.Validate(jsondata1);
+            var validate2 = schema.Validate(jsondata2);
+            var validate3 = schema.Validate(jsondata3);
+            var validate4 = schema.Validate(jsondata4);
+
+            Assert.Empty(validate1);
+            Assert.Empty(validate2);
+            Assert.Empty(validate3);
+            Assert.NotEmpty(validate4);
         }
     }
 }
