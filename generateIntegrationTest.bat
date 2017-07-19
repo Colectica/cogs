b97a481fdde0ea66a46137c34e9d0232321c4ceb
Dan Smith <dan@colectica.com>
2017-07-19 15:32:15 -0500

    initial reusable recursion test

--- generateIntegrationTest.bat
+++ generateIntegrationTest.bat
@@ -1,5 +1,6 @@
 
+del /f /s /q generated 1>nul
+rmdir /s /q generated
+
 dotnet Cogs.Console\bin\Debug\netcoreapp2.0\Cogs.Console.dll publish-cs  "cogsburger" "generated"
 dotnet Cogs.Console\bin\Debug\netcoreapp2.0\Cogs.Console.dll publish-json  "cogsburger" "generated"
-
-@pause
