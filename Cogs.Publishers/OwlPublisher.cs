9222035615f10570186fb4249db9fbd8d194a893
clementpeihengtan <clementtph@gmail.com>
2017-07-27 08:10:07 -0500

    minor changes

--- Cogs.Publishers/OwlPublisher.cs
+++ Cogs.Publishers/OwlPublisher.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) 2017 Colectica. All rights reserved
 // See the LICENSE file in the project root for more information.
+using Cogs.Model;
 using Cogs.Publishers;
+using System;
+using System.IO;
 
 namespace Cogs.Console
 {
@@ -9,5 +12,26 @@ namespace Cogs.Console
         public string CogsLocation { get; set; }
         public string TargetDirectory { get; set; }
         public bool Overwrite { get; set; }
+
+        public string TargetNamespace { get; set; } = "ddi:3_4";
+
+        public void Publish(CogsModel model)
+        {
+            //if (CogsLocation == null)
+            //{
+            //    throw new InvalidOperationException("Cogs location must be specified");
+            //}
+            if (TargetDirectory == null)
+            {
+                throw new InvalidOperationException("Target directory must be specified");
+            }
+
+            if (Overwrite && Directory.Exists(TargetDirectory))
+            {
+                Directory.Delete(TargetDirectory, true);
+            }
+
+            //Start here
+        }
     }
 }
