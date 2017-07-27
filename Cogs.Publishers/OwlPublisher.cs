779805c34dcdb0b80924590b4b20324e8b32ca8c
clementpeihengtan <clementtph@gmail.com>
2017-07-27 08:09:39 -0500

    starting owlpublisher

---
+++ Cogs.Publishers/OwlPublisher.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2017 Colectica. All rights reserved
+// See the LICENSE file in the project root for more information.
+using Cogs.Publishers;
+
+namespace Cogs.Console
+{
+    public class OwlPublisher : GraphQLPublisher
+    {
+        public string CogsLocation { get; set; }
+        public string TargetDirectory { get; set; }
+        public bool Overwrite { get; set; }
+    }
+}
