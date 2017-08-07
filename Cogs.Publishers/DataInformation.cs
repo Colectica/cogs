83145ed0802630d5f88cd8ceedb2496fdcf9d8fc
clementpeihengtan <clementtph@gmail.com>
2017-08-07 12:51:36 -0500

    change write to csv method

---
+++ Cogs.Publishers/DataInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cogs.Publishers
+{
+    public class DataInformation
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public string MinCardinality { get; set; }
+        public string MaxCardinality { get; set; }
+        public string Description { get; set; }
+        public string MinLength { get; set; }
+        public string MaxLength { get; set; }
+        public string Enumeration { get; set; }
+        public string Pattern { get; set; }
+        public string MinInclusive { get; set; }
+        public string MinExclusive { get; set; }
+        public string MaxInclusive { get; set; }
+        public string MaxExclusive { get; set; }
+        public string DeprecatedNamespace { get; set; }
+        public string DeprecatedElementOrAttribute { get; set; }
+        public string DeprecatedChoiceGroup { get; set; }
+    }
+}
--- Cogs.Publishers/ModelInitializer.cs
+++ Cogs.Publishers/ModelInitializer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cogs.Dto;
+using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -15,6 +17,7 @@ namespace Cogs.Publishers
             {
                 throw new InvalidDataException("The directory already exist");
             }
+
             //Create the directory that user want
             DirectoryInfo di = Directory.CreateDirectory(Dir);
 
@@ -30,23 +33,41 @@ namespace Cogs.Publishers
             DirectoryInfo i2 = Directory.CreateDirectory(Path.Combine(it.FullName, "item2"));
 
             //write to the directory
-            //create item1
-            StringBuilder item1 = new StringBuilder();
+            //create items
+            List<DataInformation> items = new List<DataInformation>()
+            {
+                new DataInformation()
+                {
+                    Name="name",
+                    DataType="string",
+                    MinCardinality="1",
+                    MaxCardinality="1",
+                    Description="Name of the object being referenced"
+                },
+                new DataInformation()
+                {
+                    Name="ID",
+                    DataType="string",
+                    MinCardinality="1",
+                    MaxCardinality="1",
+                    Description="ID of the object being referenced"
+                }
+            };
+            TextWriter textwriter = null;
+            CsvWriter csv = null;
+            using (textwriter = new StringWriter())
+            {
+                csv = new CsvWriter(textwriter);
+
+                csv.WriteRecords(items);
+                File.WriteAllText(Path.Combine(i1.FullName, "item1.csv"), textwriter.ToString());
+                File.WriteAllText(Path.Combine(i2.FullName, "item2.csv"), textwriter.ToString());
+            }
             StringBuilder item1readme = new StringBuilder();
-            item1.AppendLine("Name,DataType,MinCardinality,MaxCardinality,Description,MinLength,MaxLength,Enumeration,Pattern,MinInclusive,MinExclusive,MaxInclusive,MaxExclusive,DeprecatedNamespace,DeprecatedElementOrAttribute,DeprecatedChoiceGroup");
-            item1.AppendLine("name,string,1,1,name of the object being referenced.,,,,,,,,,,,");
-            item1.AppendLine("ID,string,1,1,ID of the object being referenced.,,,,,,,,,,,");
-            File.WriteAllText(Path.Combine(i1.FullName, "item1.csv"), item1.ToString());
             item1readme.Append("This is itemtype item1");
             File.WriteAllText(Path.Combine(i1.FullName, "readme.md"), item1readme.ToString());
 
-            //create item2
-            StringBuilder item2 = new StringBuilder();
             StringBuilder item2readme = new StringBuilder();
-            item2.AppendLine("Name,DataType,MinCardinality,MaxCardinality,Description,MinLength,MaxLength,Enumeration,Pattern,MinInclusive,MinExclusive,MaxInclusive,MaxExclusive,DeprecatedNamespace,DeprecatedElementOrAttribute,DeprecatedChoiceGroup");
-            item2.AppendLine("name,string,1,1,name of the object being referenced.,,,,,,,,,,,");
-            item2.AppendLine("ID,string,1,1,ID of the object being referenced.,,,,,,,,,,,");
-            File.WriteAllText(Path.Combine(i2.FullName, "item2.csv"), item2.ToString());
             item2readme.Append("This is itemtype item2");
             File.WriteAllText(Path.Combine(i2.FullName, "readme.md"), item2readme.ToString());
 
@@ -54,32 +75,76 @@ namespace Cogs.Publishers
             //Create reusabletype
             DirectoryInfo r1 = Directory.CreateDirectory(Path.Combine(rt.FullName, "Type1"));
             DirectoryInfo r2 = Directory.CreateDirectory(Path.Combine(rt.FullName, "Type2"));
-            //write to direcotry
-            //create reuse1
-            StringBuilder reuse1 = new StringBuilder();
+
+            List<DataInformation>reusableitem1 = new List<DataInformation>()
+            {
+                new DataInformation()
+                {
+                    Name="val",
+                    DataType="int",
+                    MinCardinality="1",
+                    MaxCardinality="1",
+                    Description="value of the object being referenced"
+                }
+            };
+            textwriter = null;
+            csv = null;
+            using (textwriter = new StringWriter())
+            {
+                csv = new CsvWriter(textwriter);
+                csv.WriteRecords(reusableitem1);
+                File.WriteAllText(Path.Combine(r1.FullName, "Type1.csv"), textwriter.ToString());
+            }
             StringBuilder reuse1readme = new StringBuilder();
-            reuse1.AppendLine("Name,DataType,MinCardinality,MaxCardinality,Description,MinLength,MaxLength,Enumeration,Pattern,MinInclusive,MinExclusive,MaxInclusive,MaxExclusive,DeprecatedNamespace,DeprecatedElementOrAttribute,DeprecatedChoiceGroup");
-            reuse1.AppendLine("val,int,1,1,value of the object being referenced.,,,,,,,,,,,");
-            File.WriteAllText(Path.Combine(r1.FullName, "Type1.csv"), reuse1.ToString());
             reuse1readme.Append("This is reusabletype reuse1");
             File.WriteAllText(Path.Combine(r1.FullName, "readme.md"), reuse1readme.ToString());
 
             //create reuse2
-            StringBuilder reuse2 = new StringBuilder();
+            List<DataInformation> reusableitem2 = new List<DataInformation>()
+            {
+                new DataInformation()
+                {
+                    Name="length",
+                    DataType="double",
+                    MinCardinality="1",
+                    MaxCardinality="1",
+                    Description="length of the object being referenced"
+                }
+            };
+            textwriter = null;
+            csv = null;
+            using (textwriter = new StringWriter())
+            {
+                csv = new CsvWriter(textwriter);
+                csv.WriteRecords(reusableitem2);
+                File.WriteAllText(Path.Combine(r2.FullName, "Type2.csv"), textwriter.ToString());
+            }
             StringBuilder reuse2readme = new StringBuilder();
-            reuse2.AppendLine("Name,DataType,MinCardinality,MaxCardinality,Description,MinLength,MaxLength,Enumeration,Pattern,MinInclusive,MinExclusive,MaxInclusive,MaxExclusive,DeprecatedNamespace,DeprecatedElementOrAttribute,DeprecatedChoiceGroup");
-            reuse2.AppendLine("length,double,0,1,value of the object being referenced.,,,,,,,,,,,");
-            File.WriteAllText(Path.Combine(r2.FullName, "Type2.csv"), reuse2.ToString());
             reuse2readme.Append("This is reusabletype reuse2");
             File.WriteAllText(Path.Combine(r2.FullName, "readme.md"), reuse2readme.ToString());
 
             //Create setting
-            StringBuilder identification = new StringBuilder();
-            identification.AppendLine("Name,DataType,MinCardinality,MaxCardinality,Description,MinLength,MaxLength,Enumeration,Pattern,MinInclusive,MinExclusive,MaxInclusive,MaxExclusive,DeprecatedNamespace,DeprecatedElementOrAttribute,DeprecatedChoiceGroup");
-            identification.AppendLine("ID,string,1,1,ID of the object being referenced.,,,,,,,,,,,");
+            List<DataInformation> identification1 = new List<DataInformation>()
+            {
+                new DataInformation()
+                {
+                    Name="ID",
+                    DataType="string",
+                    MinCardinality="1",
+                    MaxCardinality="1",
+                    Description="ID of the object being referenced"
+                }
+            };
+            textwriter = null;
+            csv = null;
+            using (textwriter = new StringWriter())
+            {
+                csv = new CsvWriter(textwriter);
+                csv.WriteRecords(identification1);
+                File.WriteAllText(Path.Combine(setting.FullName, "identification.csv"), textwriter.ToString());
+            }
             StringBuilder settinginfo = new StringBuilder();
             settinginfo.Append("this is the setting info csv file");
-            File.WriteAllText(Path.Combine(setting.FullName, "identication.csv"), identification.ToString());
             File.WriteAllText(Path.Combine(setting.FullName, "Settings.csv"), settinginfo.ToString());
 
             //Create topics
