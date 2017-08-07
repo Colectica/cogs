2beafdac6ddee73769c925b0e73181780f241111
Dan Smith <dan@colectica.com>
2017-08-07 12:17:15 -0500

    update Microsoft.NET.Test.Sdk

--- Cogs.Common/CogsTypes.cs
+++ Cogs.Common/CogsTypes.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) 2017 Colectica. All rights reserved
+// See the LICENSE file in the project root for more information.
+using System;
 using System.Collections.Generic;
 using System.Text;
 
