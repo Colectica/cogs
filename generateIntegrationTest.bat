3c646b689186abf8d5c993a5185f04e5f94be73f
Dan Smith <dan@colectica.com>
2022-08-02 14:33:21 -0500

    upgrade to net6.0

--- generateIntegrationTest.bat
+++ generateIntegrationTest.bat
@@ -6,8 +6,8 @@ RMDIR /S /Q "generated"
 RD /S /Q "generated"
 
 
-dotnet Cogs.Console\bin\Debug\netcoreapp2.1\cogs.dll publish-xsd  "cogsburger" "generated"
-dotnet Cogs.Console\bin\Debug\netcoreapp2.1\cogs.dll publish-cs  "cogsburger" "generated"
-dotnet Cogs.Console\bin\Debug\netcoreapp2.1\cogs.dll publish-json  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-xsd  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-cs  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-json  "cogsburger" "generated"
 
 @pause
