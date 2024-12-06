b691b6da5d4b34d56de7f671b8d7686211ead0c0
Jeremy Iverson <jeremy@colectica.com>
2024-12-06 16:34:04 +0100

    Upgrade to .NET 8.0

--- generateIntegrationTest.bat
+++ generateIntegrationTest.bat
@@ -5,10 +5,10 @@ RMDIR /S /Q "generated"
 
 RD /S /Q "generated"
 
-dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll validate "cogsburger"
-dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-xsd  "cogsburger" "generated"
-dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-cs  "cogsburger" "generated" --csproj --nullable
-dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-json  "cogsburger" "generated"
-dotnet Cogs.Console\bin\Debug\net6.0\cogs.dll publish-owl  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net8.0\cogs.dll validate "cogsburger"
+dotnet Cogs.Console\bin\Debug\net8.0\cogs.dll publish-xsd  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net8.0\cogs.dll publish-cs  "cogsburger" "generated" --csproj --nullable
+dotnet Cogs.Console\bin\Debug\net8.0\cogs.dll publish-json  "cogsburger" "generated"
+dotnet Cogs.Console\bin\Debug\net8.0\cogs.dll publish-owl  "cogsburger" "generated"
 
 @pause
